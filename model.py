e9f0c9956cd6f38393a8d317de54bc67adf01bf2
Nami Akazawa <namiakazawa@localhost.localdomain>
2019-10-11 10:02:30 -0500

    model

--- model.py
+++ model.py
@@ -56,17 +56,15 @@ class BiLSTM(nn.Module):
         if type == 0:
             self.output_layer = self.output_layer_src
             self.embedding = self.embedding_src
+            self.curr = "src"
         elif type == 1:
             self.output_layer = self.output_layer_tgt
             self.embedding = self.embedding_tgt
+            self.curr = "tgt"
         else:
             raise Exception("Invalid")
 
     def forward(self, inputs, sent_lengths):
-        batch_size, seq_len = inputs.size()
-
-        self.embedding
-    def forward(self, lang, sentences, sent_lengths):
         """
         input:
             lang: 
@@ -79,90 +77,21 @@ class BiLSTM(nn.Module):
             backward_output:
                 - 
         """
-        batch_size, seq_len = sentences.size()
-        
-        # Check which embedding to use
-        if lang == "en":
-            # Copy the embedding of the <BOS>
-            self.embedding_en.weight.data[2] = self.embedding_zh.weight.data[2]
+        # Weights for EOS shared 
+        # Also the embedding of BOS and EOS needs to be shared 
+        if self.curr == "src":
+            self.embedding.weight.data[self.src_vocab.special_tokens["BOS_FWD"]] = self.embedding_tgt.weight.data[self.src_vocab.special_tokens["BOS_FWD"]]
+            self.embedding.weight.data[self.src_vocab.special_tokens["BOS_BWD"]] = self.embedding_tgt.weight.data[self.src_vocab.special_tokens["BOS_BWD"]]
+            self.output_layer.weight.data[self.src_vocab.special_tokens["EOS"]] = self.output_layer_tgt.weight.data[self.src_vocab.special_tokens["EOS"]]
+        elif self.curr == "tgt":
+            self.embedding.weight.data[self.src_vocab.special_tokens["BOS_FWD"]] = self.embedding_src.weight.data[self.src_vocab.special_tokens["BOS_FWD"]]
+            self.embedding.weight.data[self.src_vocab.special_tokens["BOS_BWD"]] = self.embedding_src.weight.data[self.src_vocab.special_tokens["BOS_BWD"]]
+            self.output_layer.weight.data[self.src_vocab.special_tokens["EOS"]] = self.output_layer_src.weight.data[self.src_vocab.special_tokens["EOS"]]
             
-            # Copy the linear mapping weight of the <EOS> 
-            self.output_layer_en.weight.data[3] = self.output_layer_zh.weight.data[3]
-            
-            # Get the embedding 
-            # EMBEDS: (batch_size, number of sequences, embedding_dim)
-            embeds = self.embedding_en(sentences)
-            
-        elif lang == "zh":
-            # Copy the embedding of the <BOS>
-            self.embedding_zh.weight.data[2] = self.embedding_en.weight.data[2]
-            
-            # Copy the linear mapping weight of the <EOS> 
-            self.output_layer_zh.weight.data[3] = self.output_layer_en.weight.data[3]
-            
-            # Get the embedding 
-            embeds = self.embedding_zh(sentences) 
-            
-    
+        inputs = self.embedding(inputs)
+        h_t, (h_last, c_last) = self.lstm(inputs)
 
-        embeds_pad_forward = nn.utils.rnn.pack_padded_sequence(embeds, sent_lengths, batch_first=True)
-    
-        # Reverse the input and feed it to the lstm 
-        for i, s in enumerate(embeds):
-            reverse = s[:sent_lengths[i]]
-            padding = s[sent_lengths[i]:]
-            
-            # reverse the sentence (only non padding part)
-            reverse = torch.flip(reverse, [0])
+        score = self.output_layer(self.dropout(h_t))
 
-            embeds[i] = torch.cat((reverse, padding),0)
-
-            # Swap back the BOS and EOS
-            embeds[i][0], embeds[i][sent_lengths[i]-1] = embeds[i][sent_lengths[i]-1], embeds[i][0]
-
-        embeds_pad_backward = nn.utils.rnn.pack_padded_sequence(embeds, sent_lengths, batch_first=True)
-
-        """
-        inputs:
-            input : (batch, seq_len, embedding_dim)
-            h_0 : (num_layers, batch, hidden_size)
-            c_0 : (num_layers, batch, hidden_size)
-
-        returns:
-            output : (batch, seq_len, embedding_dim) 
-            h_n : (num_layers, batch, hidden_size)
-            c_n : (num_layers, batch, hidden_size)
-        """
-        # Forward
-        forward_lstm_out, (fh_t, fc_t)  = self.forward_lstm(embeds_pad_forward)
-        # Backward
-        backward_lstm_out, (bh_t, bc_t) = self.back_lstm(embeds_pad_backward
-        
-        forward_lstm_out, _ = nn.utils.rnn.pad_packed_sequence(forward_lstm_out, batch_first=True, total_length=seq_len)
-        backward_lstm_out, _ = nn.utils.rnn.pad_packed_sequence(backward_lstm_out, batch_first=True, total_length=seq_len)
-
-        # Change the shape to (batch_size * seq_len, hidden_dim) 
-        forward_lstm_out = forward_lstm_out.contiguous()
-        forward_lstm_out = forward_lstm_out.view(-1, forward_lstm_out.shape[2])
-
-        backward_lstm_out = backward_lstm_out.contiguous()
-        backward_lstm_out = backward_lstm_out.view(-1, backward_lstm_out.shape[2])
-
-        # Dropout 
-        forward_lstm_out = self.dropout(forward_lstm_out)
-        backward_lstm_out = self.dropout(backward_lstm_out)
-
-        # Check which linear mapping to use
-        output_layer = self.output_layer_en if lang == "en" else self.output_layer_zh
-
-        forward_output =  output_layer(forward_lstm_out)
-        backward_output = output_layer(backward_lstm_out)
-
-        # shape = (batch_size * seq_len, vocab_size)
-        forward_output, backward_output = F.log_softmax(forward_output, dim=1), F.log_softmax(backward_output, dim=1)
-
-        # shape = (batch_size, seq_len, vocab_size)
-        # return forward_output.view(batch_size, seq_len, -1), backward_output.view(batch_size, seq_len, -1)
-        
-        return forward_output, backward_output
+        return score 
 
